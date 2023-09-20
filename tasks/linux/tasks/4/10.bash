815d64e840ba235342337e445c7009824514bbb8
Нина Мельникова <melnikova@MacBook-Pro-Nina.local>
2023-09-20 21:59:20 +0300

    new

--- tasks/linux/tasks/4/10.bash
+++ tasks/linux/tasks/4/10.bash
@@ -1,2 +1,2 @@
- Выведи мне топ 5 файлов, в которых чаще всего встречается слово pizza
-grep -r -w -o 'pizza' . | sort -nr | head -5
+
+grep -r -w -o 'pizza' . | sort -nr | uniq -c | head -5
--- tasks/linux/tasks/4/6.bash
+++ tasks/linux/tasks/4/6.bash
@@ -1,2 +1,1 @@
- Найдите все py файлы, содержащие в названии любое из трёх слов: backdoor, trojan, keylogger.
-find / -type f -name "*.py" -a name "*backdoor.py" -a name "*trojan.py" -a name "*keylogger.py"
+find / -type f -name "*.py" | grep -iE "backdoor|trojan|keylogger"
