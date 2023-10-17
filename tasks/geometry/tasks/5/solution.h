1acf590d7321cae9c0f9e200da7069ba453bc807
Нина Мельникова <melnikova@MacBook-Pro-Nina.local>
2023-10-17 11:13:43 +0300

    lolo

--- tasks/geometry/tasks/5/solution.h
+++ tasks/geometry/tasks/5/solution.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <stdio.h>
-#include <stdlib.h>
 
 int Mod(int x, int y) {
     if (y != 0) {
@@ -45,14 +44,18 @@ int Abs(int x) {
     }
 }
 
-const int x_0 = 10;
-const int y_0 = 10;
-const int rSmall = 5;
-const int rBig = 10;
-const int i_0 = -1;
-const int j_0 = -1;
-const int l_0 = -9;
+const int X_0 = 10;
+const int Y_0 = 10;
+const int R_SMALL = 5;
+const int R_BIG = 10;
+const int I_0 = -1;
+const int J_0 = -1;
+const int L_0 = -9;
 const int ITERATIONS = 50;
+const int TWENTY = 20;
+const int TWENTYFIVE = 25;
+const int THIRTY = 30;
+const int ONE = 1;
 
 typedef struct {
     int x;
@@ -61,8 +64,8 @@ typedef struct {
 
 int CheckZone(Point p) {
     int cord_x = p.x, cord_y = p.y;
-    if (((cord_x - x_0) * (cord_x - x_0) + (cord_y - y_0) * (cord_y - y_0) <= rBig * rBig) &&
-        ((cord_x - x_0) * (cord_x - x_0) + (cord_y - y_0) * (cord_y - y_0) >= rSmall * rSmall)) {
+    if (((cord_x - X_0) * (cord_x - X_0) + (cord_y - Y_0) * (cord_y - Y_0) <= R_BIG * R_BIG) &&
+        ((cord_x - X_0) * (cord_x - X_0) + (cord_y - Y_0) * (cord_y - Y_0) >= R_SMALL * R_SMALL)) {
         return 1;
     } else {
         return 0;
@@ -70,29 +73,29 @@ int CheckZone(Point p) {
 }
 
 int Task() {
-    int i = i_0;
-    int j = j_0;
-    int l = l_0;
+    int i = I_0;
+    int j = J_0;
+    int l = L_0;
     Point p;
     p.x = i;
     p.y = j;
 
-    int countBelong = 0;  // Счетчик точек, принадлежащих зоне
+    int count_belong = 0;  // Счетчик точек, принадлежащих зоне
 
-    for (int k = 1; k <= ITERATIONS; ++k) {
-        i = Max(p.y - k, l - k) % 30 + Max(p.x + l, p.y + k) % 20;
-        j = (Abs(p.x - l) * Sign(p.y + k) + Abs(p.x - k) * (p.y + k)) % 20;
-        l = ((p.x + k) * (p.y - k) * (l + k)) % 25;
+    for (int k = ONE; k <= ITERATIONS; ++k) {
+        i = Max(p.y - k, l - k) % THIRTY + Max(p.x + l, p.y + k) % TWENTY;
+        j = (Abs(p.x - l) * Sign(p.y + k) + Abs(p.x - k) * (p.y + k)) % TWENTY;
+        l = ((p.x + k) * (p.y - k) * (l + k)) % TWENTYFIVE;
         p.x = i;
         p.y = j;
         printf("k = %d, x = %d, y = %d, l = %d belong = %d \n", k, p.x, p.y, l, CheckZone(p));
 
         if (CheckZone(p)) {
-            countBelong++;
+            count_belong++;
         }
     }
 
-    printf("Number of points belonging to the zone: %d\n", countBelong);
+    printf("Number of points belonging to the zone: %d\n", count_belong);
 
     return 0;
 }
